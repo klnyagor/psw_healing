9a196e1a916bfbba3c37c22f27712938defb319f
klnyagor <kalenieves.yagor@academico.ifpb.edu.br>
2024-04-22 03:09:18 -0300

    aula 3

--- medico/views.py
+++ medico/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Especialidades, DadosMedico, is_medico, DatasAbertas
-from paciente.models import Consulta
+from paciente.models import Consulta, Documento
 from django.contrib.messages import constants, add_message
 from datetime import datetime, timedelta
 
@@ -13,7 +13,7 @@ def cadastro_medico(request):
 
   if request.method == "GET":
     especialidades = Especialidades.objects.all()
-    return render(request, "cadastro_medico.html", {'especialidades': especialidades})
+    return render(request, "cadastro_medico.html", {'especialidades': especialidades, 'is_medico': is_medico(request.user)})
   elif request.method == "POST":
     crm = request.POST.get('crm')
     nome = request.POST.get('nome')
@@ -55,8 +55,8 @@ def abrir_horario(request):
   
   if request.method == "GET":
     dados_medico = DadosMedico.objects.get(user=request.user)
-    datas_abertas = DatasAbertas.objects.filter(user=request.user)
-    return render(request, 'abrir_horario.html', {'dados_medicos': dados_medico, 'datas_abertas': datas_abertas})
+    datas_abertas = DatasAbertas.objects.filter(user=request.user).order_by('data')
+    return render(request, 'abrir_horario.html', {'dados_medicos': dados_medico, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
   
   elif request.method == "POST":
     data = request.POST.get('data')
@@ -80,7 +80,72 @@ def consultas_medico(request):
 
  hoje = datetime.now().date()
  consultas_hoje = Consulta.objects.filter(data_aberta__user=request.user).filter(data_aberta__data__gte=hoje).filter(data_aberta__data__lt=hoje + timedelta(days=1)).order_by('data_aberta__data')
- consultas_restantes = Consulta.objects.exclude(id__in=consultas_hoje.values('id')).order_by('data_aberta__data')
+ consultas_restantes = Consulta.objects.exclude(id__in=consultas_hoje.values('id')).filter(data_aberta__user=request.user).order_by('data_aberta__data')
  
  return render(request, 'consultas_medico.html', {'consultas_hoje': consultas_hoje, 'consultas_restantes': consultas_restantes, 'is_medico': is_medico(request.user)})
 
+def consulta_area_medico(request, id_consulta):
+  if not is_medico(request.user):
+    add_message(request, constants.WARNING, 'Somente médicos podem acessar essa página.')
+    return redirect('/usuarios/logout')
+  
+  if request.method == "GET":
+    consulta = Consulta.objects.get(id=id_consulta)
+    documentos = Documento.objects.filter(consulta=consulta)
+    return render(request, 'consulta_area_medico.html', {'consulta': consulta, 'documentos': documentos,'is_medico': is_medico(request.user)})
+  elif request.method == "POST":
+    consulta = Consulta.objects.get(id=id_consulta)
+    link = request.POST.get('link')
+    if consulta.status == 'C':
+      add_message(request, constants.WARNING, 'Essa consulta já foi cancelada, você não pode inicia-la')
+      return redirect(f'/medicos/consulta_area_medico/{id_consulta}')
+    elif consulta.status == "F":
+      add_message(request, constants.WARNING, 'Essa consulta já foi finalizada, você não pode inicia-la')
+      return redirect(f'/medicos/consulta_area_medico/{id_consulta}')
+    
+    consulta.link = link
+    consulta.status = 'I'
+    consulta.save()
+    add_message(request, constants.SUCCESS, 'Consulta inicializada com sucesso.')
+    return redirect(f'/medicos/consulta_area_medico/{id_consulta}')
+    
+def finalizar_consulta(request, id_consulta):
+   if not is_medico(request.user):
+        add_message(request, constants.WARNING, 'Somente médicos podem finalizar consultas.')
+        return redirect('/usuarios/logout')
+   
+   consulta=Consulta.objects.get(id=id_consulta)
+   if request.user != consulta.data_aberta.user:
+      add_message(request, constants.ERROR, "Você não possui autorização para finalizar essa consulta!")
+      return redirect("/medicos/consultas_medico/")
+   
+   consulta.status = "F"
+   consulta.save()
+   return redirect(f"/medicos/consulta_area_medico/{id_consulta}")
+
+def add_documento(request, id_consulta):
+  if not is_medico(request.user):
+    add_message(request, constants.WARNING, 'Somente médicos podem acessar essa página.')
+    return redirect('/usuarios/logout')
+  
+  consulta = Consulta.objects.get(id=id_consulta)
+  
+  if consulta.data_aberta.user != request.user:
+    add_message(request, constants.ERROR, 'Essa consulta não é sua!')
+    return redirect(f'/medicos/consulta_area_medico/{id_consulta}')
+  
+  titulo = request.POST.get('titulo')
+  documento = request.FILES.get('documento')
+  
+  if not documento:
+    add_message(request, constants.WARNING, 'Adicione o documento.')
+    return redirect(f'/medicos/consulta_area_medico/{id_consulta}')
+  
+  documento = Documento(
+    consulta=consulta,
+    titulo=titulo,
+    documento=documento
+  )
+  documento.save()
+  add_message(request, constants.SUCCESS, 'Documento enviado com sucesso!')
+  return redirect(f'/medicos/consulta_area_medico/{id_consulta}')
--- paciente/views.py
+++ paciente/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.messages import add_message, constants
-from medico.models import DadosMedico, Especialidades, DatasAbertas
-from .models import Consulta
+from medico.models import DadosMedico, Especialidades, DatasAbertas, is_medico
+from .models import Consulta, Documento
 from datetime import datetime
 
 # Create your views here.
@@ -20,13 +20,13 @@ def home(request):
   if especialidades_filtrar:
     medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
 
-  return render(request, "home.html", {'medicos': medicos, 'especialidades': especialidades})
+  return render(request, "home.html", {'medicos': medicos, 'especialidades': especialidades, 'is_medico': is_medico(request.user)})
 
 def escolher_horario(request, id_dados_medicos):
   if request.method == "GET":
     medico = DadosMedico.objects.get(id=id_dados_medicos)
     datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False).order_by('data')
-    return render(request, "escolher_horario.html", {"medico": medico, "datas_abertas": datas_abertas})
+    return render(request, "escolher_horario.html", {"medico": medico, "datas_abertas": datas_abertas, 'is_medico': is_medico(request.user)})
   
 def agendar_horario(request, id_data_aberta):
   if request.method == 'GET':
@@ -47,6 +47,12 @@ def minhas_consultas(request):
 
   if request.method == "GET":
     minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now()).order_by('data_aberta__data')
-    return render(request, "minhas_consultas.html", {"minhas_consultas": minhas_consultas})
+    return render(request, "minhas_consultas.html", {"minhas_consultas": minhas_consultas, 'is_medico': is_medico(request.user)})
 
+def consulta(request, id_consulta):
+ if request.method == 'GET':
+  consulta = Consulta.objects.get(id=id_consulta)
+  dado_medico = DadosMedico.objects.get(user=consulta.data_aberta.user)
+  documentos = Documento.objects.filter(consulta=consulta)
+  return render(request, 'consulta.html', {'consulta': consulta, 'dado_medico': dado_medico, 'documentos': documentos, 'is_medico': is_medico(request.user)})
 
